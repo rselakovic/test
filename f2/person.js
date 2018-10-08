92bf0fe510613f7225c6d9c85a1052f05ce29500
Dragan Pirkovic <Dragan.Pirkovic@mht.net>
2018-10-08 14:50:44 +0200

    opet sam komitovao neke gluposti

--- f2/person.js
+++ f2/person.js
@@ -1,8 +1,9 @@
 var person = {
     firstName:"Pera",
     lastName: "Peric",
+	nicknaname : "Petko",
     fullName: function () {
-        return this.firstName + " " + this.lastName;
+        return this.firstName + " " + this.lastName+ " " + this.nicknaname;
     }
 }
 person.fullName(); 
